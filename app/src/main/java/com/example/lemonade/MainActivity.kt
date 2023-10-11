36518b9984e80708203dc83d42a87ba34c60e899
Mark Pralat <mpralat@gmail.com>
2023-10-11 14:53:55 -0400

    Got the image background color and rounded corners to work.

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.lemonade
 
+import android.graphics.drawable.shapes.RoundRectShape
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,12 +8,15 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -25,7 +29,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Alignment.Companion.TopEnd
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -46,7 +52,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Lemonade()
+                    LemonadeInBox()
                 }
             }
         }
@@ -88,7 +94,8 @@ fun Lemonade(modifier: Modifier = Modifier) {
     ) {
 
         Row(
-            modifier = modifier.background(Color.Yellow)
+            modifier = modifier
+                .background(Color.Yellow)
                 .height(48.dp)
                 .fillMaxWidth(1f),
             horizontalArrangement = Arrangement.Center,
@@ -106,7 +113,6 @@ fun Lemonade(modifier: Modifier = Modifier) {
         }
 
         Image(
-
             painter = painterResource(imageResource),
             contentDescription = step_contentDescription.toString(),
 //            TODO alignment = CenterVertically,
@@ -120,10 +126,95 @@ fun Lemonade(modifier: Modifier = Modifier) {
     }
 }
 
+// Try with a Box - will it help get the row at the top?
+@Composable
+fun LemonadeInBox(modifier: Modifier = Modifier) {
+    var make_lemonade_step by remember { mutableStateOf(1) }
+
+    // only needed in step 2 when squeezing the lemon
+    var squeezeCount = (2..4).random()
+
+//        var squeezeCount = when (make_lemonade_step) {
+//            2 -> {
+//                (2..4).random()
+//            }
+//            else -> 1
+//        }
+
+    println("squeezeCount: $squeezeCount")
+
+    var step_instruction = when (make_lemonade_step) {
+        1 -> R.string.Lemon_tree
+        2 -> R.string.Lemon
+        3 -> R.string.Glass_of_lemonade
+        else -> R.string.Empty_glass
+    }
+
+    var step_contentDescription = when (make_lemonade_step) {
+        1 -> R.string.Lemon_tree_description
+        2 -> R.string.Lemon_description
+        3 -> R.string.Glass_description
+        else -> R.string.Empty_glass_description
+    }
+
+    when (make_lemonade_step) {
+        2 -> squeezeCount = 3 /// squeezeCount.random(2..4)
+        else -> squeezeCount = 1
+    }
+
+    val imageResource = when (make_lemonade_step) {
+        1 -> R.drawable.lemon_tree
+        2 -> R.drawable.lemon_fruit
+        3 -> R.drawable.lemon_drink
+        else -> R.drawable.lemon_empty_glass
+    }
+
+    Box {
+        Row(
+            modifier = modifier
+                .background(Color.Yellow)
+                .height(48.dp)
+                .fillMaxWidth(1f),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = CenterVertically,
+        ) {
+            Text(
+                text = stringResource(id = R.string.app_name),
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+            )
+        }
+    }
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(imageResource),
+            contentDescription = step_contentDescription.toString(),
+            modifier = Modifier
+//                .size(300.dp)
+                .clip(RoundedCornerShape(26.dp))
+                .size(250.dp)
+//                .background(color = Color(0xFFE0FFFF), shape = RoundRectShape())
+                .background(color = Color(0xFFE0FFFF))
+
+                .clickable { make_lemonade_step = (make_lemonade_step+1) % 4 },
+        )
+        Spacer(modifier = Modifier.height(36.dp))
+        Text(
+            text = stringResource(id = step_instruction),
+            modifier = modifier
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     LemonadeTheme {
-        Lemonade()
+        LemonadeInBox()
     }
 }
--- app/src/main/java/com/example/lemonade/ui/theme/Theme.kt
+++ app/src/main/java/com/example/lemonade/ui/theme/Theme.kt
@@ -22,7 +22,7 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
+    primary = Red // Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
 
